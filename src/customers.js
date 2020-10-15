868f82e5eef953f9655932e64e5bffba66867af6
trxrse12 <trxrse@gmail.com>
2020-10-15 09:05:32 +0100

    15/Oct/2020 H3. 1. Customers class now has a  method all which returns all provided customers

--- src/customers.js
+++ src/customers.js
@@ -1,5 +1,17 @@
 export class Customers {
+  constructor(){
+    this.nextId = 0;
+    this.customers = {};
+    // this.add = this.add.bind(this);
+  }
+
   add(customer) {
-    return customer
+    const customerWithId = Object.assign({}, customer, {id: this.nextId++});
+    this.customers[customerWithId.id] = customerWithId;
+    return customerWithId;
+  }
+
+  all() {
+    return Object.assign({}, this.customers);
   }
 }
--- test/customers.test.js
+++ test/customers.test.js
@@ -14,5 +14,28 @@ describe('customers', () => {
       expect(result.lastName).toBe('Jones');
       expect(result.phoneNumber).toBe('123456789');
     });
+
+    it('generates different ids to each customer', () => {
+      const customers = new Customers();
+      const first = customers.add(customer);
+      const second = customers.add(customer);
+      expect(first.id).not.toBe(second.id);
+    });
+  });
+
+  describe('all', () => {
+    const one = {firstName: 'Ashley'};
+    const two = {firstName: 'Sam'};
+
+    it('returns all added customers', () => {
+      const customers = new Customers();
+      const oneId = customers.add(one).id;
+      const twoId = customers.add(two).id;
+
+      const result = customers.all();
+
+      expect(result[oneId].firstName).toEqual(one.firstName);
+      expect(result[twoId].firstName).toEqual(two.firstName);
+    });
   });
 });
